b62788bc882fb8fe6c8ecd754f581934ae6903d6
Bill Bristow <wab5217@psu.edu>
2025-03-24 14:18:35 -0400

    Moved c files out of include statements, added typedeffs to header file created makefile  Please enter the commit message for your changes. Lines starting

--- c_include/clear_freq_search.h
+++ c_include/clear_freq_search.h
@@ -1,2 +1,40 @@
+#include <stdbool.h>
+
+#ifndef CLR_BANDS_MAX
+#define CLR_BANDS_MAX 6
+#endif
+
+#define VERBOSE 1
+
+typedef struct sample_meta_data {
+    int *antenna_list;
+    int num_antennas;
+    int number_of_samples;
+    double x_spacing;
+    int usrp_rf_rate;
+    int usrp_fcenter;
+} sample_meta_data;
+
+typedef struct freq_data {
+    double *restricted_freq;
+    double *clear_freq_range;
+} freq_data;
+
+typedef struct freq_band {
+    int f_start;
+    int f_end;
+    double noise;
+    bool is_selected;
+} freq_band;
+
+
+typedef struct clear_freq {
+    double noise;
+    double tfreq;
+} clear_freq;
+
+
 #pragma once
 void phasing_and_beamforming(double beam_angle, int *clear_freq_range, sample_meta_data *meta_data, fftw_complex *phasing_vector, int *antennas, int num_samples, fftw_complex **raw_samples, int **sample_im, int **sample_re, fftw_complex *beamformed_samples);
+
+
--- c_include/misc_read_writes.c
+++ c_include/misc_read_writes.c
@@ -10,39 +10,7 @@
 #include "ini_parser.c"
 #include "read_config.c"
 #include <time.h>
-
-
-#ifndef CLR_BANDS_MAX
-#define CLR_BANDS_MAX 6
-#endif
-
-
-typedef struct sample_meta_data {
-    int *antenna_list;
-    int num_antennas;
-    int number_of_samples;
-    double x_spacing;
-    int usrp_rf_rate;
-    int usrp_fcenter;
-} sample_meta_data;
-
-typedef struct freq_data {
-    double *restricted_freq;
-    double *clear_freq_range;
-} freq_data;
-
-typedef struct freq_band {
-    int f_start;
-    int f_end;
-    double noise;
-    bool is_selected;
-} freq_band;
-
-
-typedef struct clear_freq {
-    double noise;
-    double tfreq;
-} clear_freq;
+#include "clear_freq_search.h"
 
 void file_access_error(const char *filepath) {
     printf("[CFS] ERROR: accessing filepath: %s\n", filepath);
