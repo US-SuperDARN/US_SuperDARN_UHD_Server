ef533898fe61eeb69511bf2a325e2a23f35b33fc
Hearing <gameblamer0@gmail.com>
2025-07-24 13:52:52 +0000

    Reverted attempt to use replay_buffer in the driver. Used delay in streaming to tx_worker baased on length of time needed

--- usrp_driver/usrp_driver.cpp
+++ usrp_driver/usrp_driver.cpp
@@ -563,7 +563,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
        return -1;
     }
     stream_args.channels = channel_numbers;
-    stream_args.args["streamer"]="replay_buffered";    
+
     uhd::rx_streamer::sptr rx_stream = usrp->get_rx_stream(stream_args);
     uhd::tx_streamer::sptr tx_stream = usrp->get_tx_stream(stream_args);
 
@@ -902,7 +902,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[]){
 			useconds_t usecs=1000;
 			usleep(usecs);			
                         if (tx_worker_active) {
-			  uhd_threads.create_thread(boost::bind(usrp_tx_worker, tx_stream, &tx_samples, num_samples_per_pulse_with_padding, start_time, pulse_sample_idx_offsets, txrate)); 
+			  uhd_threads.create_thread(boost::bind(usrp_tx_worker, tx_stream, &tx_samples, num_samples_per_pulse_with_padding, start_time, pulse_sample_idx_offsets,txrate)); 
                         }
 
 			usleep(usecs);
