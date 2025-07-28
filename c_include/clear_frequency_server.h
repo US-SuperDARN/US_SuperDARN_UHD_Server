5310e432784c64591709f73059ca74103766d5ac
Hearing <gameblamer0@gmail.com>
2025-07-28 18:44:39 +0000

    Doc: Adjust CFS Terminal Log Level
    - Adjust CFS terminal log level from 0 (trace) to 1 (debug) for readability

--- c_include/clear_frequency_server.h
+++ c_include/clear_frequency_server.h
@@ -9,7 +9,7 @@
 
 
 // Logging Vars
-#define LOG_TERMINAL_LEVEL  0                           // 0 = TRACE, 1 = DEBUG, 2 = INFO, 3 = WARN, 4 = ERROR, 5 = FATAL  
+#define LOG_TERMINAL_LEVEL  1                           // 0 = TRACE, 1 = DEBUG, 2 = INFO, 3 = WARN, 4 = ERROR, 5 = FATAL  
 #define LOG_FILE_LEVEL      0
 #define LOG_PREFIX          "[CFS] %s"                  // *Unused* Prefix for log messages
 #define LOG_FILEPATH        "log/cfs/cfs.%s.log"
