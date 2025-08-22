bf0cb09eb376c37a973238c653e3bc3788b282b0
egthomas <evan.g.thomas@dartmouth.edu>
2025-08-22 15:58:12 -0400

    Trying to fix bug in clear search range start and end values written to csv files

--- c_include/misc_read_writes.h
+++ c_include/misc_read_writes.h
@@ -20,8 +20,8 @@ void write_spectrum_mag_csv(char *filename, double *spectrum, double *freq_vecto
 void write_spectrum_mag_bin(char *filename, double *spectrum, double *freq_vector, int num_samples);
 
 // Clear frequency writing
-void write_clr_freq_csv(char *filename, freq_band *clr_band);
-void write_clr_freq_bin(char *filename, freq_band *clr_band);
+void write_clr_freq_csv(char *filename, freq_band *clr_band, int *clr_range);
+void write_clr_freq_bin(char *filename, freq_band *clr_band, int *clr_range);
 
 // Sample writing
 void write_sample_mag_csv(char *filename, int **raw_samples_mag, double *freq_vector, sample_meta_data *meta_data);
