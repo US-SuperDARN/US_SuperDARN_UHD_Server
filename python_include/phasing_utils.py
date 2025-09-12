3d292ef0c285f25f26325b937382749c638ad260
egthomas <evan.g.thomas@dartmouth.edu>
2025-09-12 13:12:59 -0400

    Modifying phasing_utils.py to clean up spacing

--- python_include/phasing_utils.py
+++ python_include/phasing_utils.py
@@ -4,13 +4,14 @@
 import numpy as np
 
 from scipy.constants import speed_of_light as C
-#C = 3e8
 
 verbose = False
+
 
 # returns a complex number from a phase in radians
 def rad_to_rect(rad):
     return np.exp(-1j * rad)
+
 
 # nbeams - total number of beams
 # beamnum - beam number to calculate azm
@@ -21,28 +22,30 @@ def calc_beam_azm_rad(nbeams, beamnum, beam_sep):
     center_beam = (nbeams - 1.0) / 2.
 
     # calculate beam azimuth, in radians
-  #  bmazm = np.deg2rad(90 + (beamnum - center_beam) * beam_sep)
     bmazm = np.deg2rad( (beamnum - center_beam) * beam_sep)
     if verbose:
         print("nBeams: {}, beamNum: {}, beam_sep: {}".format(nbeams, beamnum, beam_sep))
         print("  beam = {} degree".format(bmazm / np.pi*180))
     return bmazm
 
+
 # calculates the phase increment between antennas
-# to produce a mainlobe steering of bmazm at tfreq 
+# to produce a mainlobe steering of bmazm at tfreq
 # tfreq in hz, bmazm in radians
 def calc_phase_increment(bmazm, tfreq, x_spacing):
     # translate to phase increment
     wavelength = C / tfreq
     pshift = (2 * np.pi * x_spacing * np.sin(bmazm)) / wavelength
     if verbose:
-        print("tfreq: {} x_spacing: {}\n pshift: {} degree".format(tfreq, x_spacing, pshift*180/np.pi))  
-    return pshift 
+        print("tfreq: {} x_spacing: {}\n pshift: {} degree".format(tfreq, x_spacing, pshift*180/np.pi))
+    return pshift
+
 
 # calculates a complex number for a phase shift
 def calc_beamforming_phase_rect(antenna, pshift):
     beamforming_phase = rad_to_rect(antenna * pshift)
     return beamforming_phase
+
 
 def beamform_uhd_samples(samples, phasing_matrix, n_samples, antennas, pack_uint32):
     beamformed_samples = np.ones(n_samples, dtype=np.complex_)
